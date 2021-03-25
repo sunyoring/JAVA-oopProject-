b03ada080ff3406a978e16048558c82481152fdb
sunyoring <78639649+sunyoring@users.noreply.github.com>
2021-03-25 14:16:54 +0900

    BlockExam

--- src/kh/oop1/day03/exam/BlockExam.java
+++ src/kh/oop1/day03/exam/BlockExam.java
@@ -1,9 +1,9 @@
 package kh.oop1.day03.exam;
 
 public class BlockExam {
-	public static String name = "홍길동";
+	public static String name = "홍길동"; //명시적 초기화
 	
-	public BlockExam() {
+	public BlockExam() { 
 		System.out.println("생성자 영역");
 	}
 	int age = 20;
@@ -15,7 +15,7 @@ public class BlockExam {
 		System.out.println("static 블럭 영역");
 		name = "박길동";
 	}
-	{
+	{ // 생성자 이전에 값을 초기화할 때 사용한다.
 		System.out.println(age);
 		System.out.println("인스턴스 블럭1");
 		this.age = 21;
