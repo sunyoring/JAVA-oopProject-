7d62b27d225436bebc4f516e66a7f1c9131fa0a6
sunyoring <78639649+sunyoring@users.noreply.github.com>
2021-03-29 00:35:08 +0900

    Overriding

--- src/kh/p2/over/run/Run.java
+++ src/kh/p2/over/run/Run.java
@@ -6,15 +6,28 @@ import com.kh.p2.over.parent.model.vo.Shop;
 public class Run {
 
 	public static void main(String[] args) {
-		Shop sh = new Shop("홍길동","수원시 인계동");
-		System.out.println(sh); 
+		Shop sh = new Shop("홍길동", "수원시 인계동");
+		System.out.println(sh);
 		// toString을 오버라이딩하는 이유 : 객체 명만 써도 자동으로 객체 내의 toString을 찾아 문자열로 출려해준다.
-		
+
 		sh.order("부모 메소드 >> ");
-		
-		CoffeeShop cs = new CoffeeShop("홍길동","수원시 인계동","아메리카노",2000,"스타벅스");
-		
+
+		CoffeeShop cs = new CoffeeShop("홍길동", "수원시 인계동", "아메리카노", 2000, "스타벅스");
+
 		cs.order("오버라이딩에 의한 자식 메소드 >> ");
+
+		CoffeeShop cs2 = new CoffeeShop("핫초코", 3000, "커피빈");
+		CoffeeShop cs3 = new CoffeeShop("핫초코", 3000, "커피빈");
+
+		// hashCode : 객체의 주소값을 정수형으로 변환
+		System.out.println(cs2.hashCode());
+		System.out.println(cs3.hashCode());
+		// 두 객체는 주소값이 다르다.
+
+		// equals메소드를 오버라이딩하여 주소값이 다른 두 객체 내부의 값이 같다면 true를 반환하도록 한다.
+
+		System.out.println("equals의 비교 결과 : " + cs2.equals(cs3));
+
 	}
 
 }
