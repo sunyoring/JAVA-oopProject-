e0d525274b88523f590a010190732ab7891d0966
sunyoring <78639649+sunyoring@users.noreply.github.com>
2021-03-31 19:54:50 +0900

    Update clean code

--- src/kh/prac4/run/Run.java
+++ src/kh/prac4/run/Run.java
@@ -11,27 +11,36 @@ public class Run {
 				new Member("김가람", 27, '남', 'B'), 
 				new Member("박수연", 25, '여', 'O') };
 
-		Member[] tempM = new Member[8]; // 객체배열을 기본생성자로 초기화한 객체로 채움.
-		for (int i = 0; i < tempM.length; i++) {
-			tempM[i] = new Member();
+//		Member[] tempM = new Member[8]; // 객체배열을 기본생성자로 초기화한 객체로 채움.
+//		for (int i = 0; i < tempM.length; i++) { //성별과 혈액형이 일치하는 경우의 수를 배열에 담는다.
+//			tempM[i] = new Member();			// 총 경우의 수가 8개 이므로 8개의 배열 선언.
+//
+//		}
+//		int tempIndex = 0; //
+//		for (int i = 0; i < m.length; i++) { // 기준이 되는 인원을 바꿀 for문
+//			for (int j = i+1; j< m.length; j++) {// 나머지 인원을 비교하는 for문
+//				if (m[i].equals(m[j])) {
+//					int k;
+//					for (k = 0; k < tempM.length; k++) { //tempM 객체배열을 돌려가며 확인
+//						if (m[j].equals(tempM[k])) { //중복값이면 for문을 돌리지 않음.
+//							break;
+//						}
+//					}
+//					if (k == tempM.length) { 
+//						tempM[tempIndex++] = m[j];//tempM에 값을 넣는 코드//tempM에 값을 넣는 코드
+//					}
+//				}
+//			}
+//		}
 
-		}
-		int tempIndex = 0; //
-		for (int i = 0; i < m.length; i++) { // 기준이 되는 인원을 바꿀 for문
-			for (int j = i+1; j< m.length; j++) {// 나머지 인원을 비교하는 for문
-				if (m[i].equals(m[j])) {
-					int k;
-					for (k = 0; k < tempM.length; k++) { //tempM 객체배열을 돌려가며 확인
-						if (m[j].equals(tempM[k])) { //중복값이면 for문을 돌리지 않음.
-							break;
-						}
-					}
-					if (k == tempM.length) { 
-						tempM[tempIndex++] = m[j]; //tempM에 값을 넣는 코드
-					}
-				}
+//		System.out.println("성별과 혈액형이 둘 다 일치하는 경우의 수는 총 " + tempIndex+"가지" );
+		int count = 0;
+		for(int i = 0; i < m.length; i++) {
+			for(int j=i+1; j<m.length; j++) {
+				if(m[i].equals(m[j]))
+					count++;
 			}
 		}
-		System.out.println("성별과 혈액형이 둘 다 일치하는 경우의 수는 총 " + tempIndex+"가지" );
+		System.out.println(count);
 	}
 }
